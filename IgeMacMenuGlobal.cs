0cc3f06fa27b63d448c6cb25733c6907612bb58d
carlokok <carlokok@24605aea-0a50-11df-a45b-0370d43aa155>
2010-01-26 19:34:48 +0000

    0: Use the original C# code, for ease of updating
    git-svn-id: http://code.remobjects.com/svn/monohelpers/trunk/Source/RemObjects.Mono.Helpers@16 24605aea-0a50-11df-a45b-0370d43aa155

---
+++ IgeMacMenuGlobal.cs
@@ -0,0 +1,69 @@
+// Origin: http://anonsvn.mono-project.com/viewvc/trunk/mono-tools/docbrowser/macbuild/IgeMacMenuGlobal.cs
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace RemObjects.Mono.Helpers
+{
+
+	public class IgeMacMenu {
+
+		[DllImport("libRemObjects.Mono.Helpers.dylib")]
+		static extern void ige_mac_menu_connect_window_key_handler (IntPtr window);
+
+		public static void ConnectWindowKeyHandler (Gtk.Window window)
+		{
+			ige_mac_menu_connect_window_key_handler (window.Handle);
+		}
+
+		[DllImport("libRemObjects.Mono.Helpers.dylib")]
+		static extern void ige_mac_menu_set_global_key_handler_enabled (bool enabled);
+
+		public static bool GlobalKeyHandlerEnabled {
+			set { 
+				ige_mac_menu_set_global_key_handler_enabled (value);
+			}
+		}
+
+		[DllImport("libRemObjects.Mono.Helpers.dylib")]
+		static extern void ige_mac_menu_set_menu_bar(IntPtr menu_shell);
+
+		public static Gtk.MenuShell MenuBar { 
+			set {
+				ige_mac_menu_set_menu_bar(value == null ? IntPtr.Zero : value.Handle);
+			}
+		}
+
+		[DllImport("libRemObjects.Mono.Helpers.dylib")]
+		static extern void ige_mac_menu_set_quit_menu_item(IntPtr quit_item);
+
+		public static Gtk.MenuItem QuitMenuItem { 
+			set {
+				ige_mac_menu_set_quit_menu_item(value == null ? IntPtr.Zero : value.Handle);
+			}
+		}
+
+		[DllImport("libRemObjects.Mono.Helpers.dylib")]
+		static extern IntPtr ige_mac_menu_add_app_menu_group();
+
+		public static RemObjects.Mono.Helpers.IgeMacMenuGroup AddAppMenuGroup() {
+			IntPtr raw_ret = ige_mac_menu_add_app_menu_group();
+			RemObjects.Mono.Helpers.IgeMacMenuGroup ret = raw_ret == IntPtr.Zero ? null : (RemObjects.Mono.Helpers.IgeMacMenuGroup) GLib.Opaque.GetOpaque (raw_ret, typeof (RemObjects.Mono.Helpers.IgeMacMenuGroup), false);
+			return ret;
+		}
+	}
+
+	public class IgeMacMenuGroup : GLib.Opaque {
+
+		[DllImport("libRemObjects.Mono.Helpers.dylib")]
+		static extern void ige_mac_menu_add_app_menu_item(IntPtr raw, IntPtr menu_item, IntPtr label);
+
+		public void AddMenuItem(Gtk.MenuItem menu_item, string label) {
+			IntPtr native_label = GLib.Marshaller.StringToPtrGStrdup (label);
+			ige_mac_menu_add_app_menu_item(Handle, menu_item == null ? IntPtr.Zero : menu_item.Handle, native_label);
+			GLib.Marshaller.Free (native_label);
+		}
+
+		public IgeMacMenuGroup(IntPtr raw) : base(raw) {}
+	}
+}
---
+++ Utilities.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RemObjects.Mono.Helpers
+{
+    static class Utilities
+    {
+        public static string HomePath { get { return Environment.GetFolderPath(Environment.SpecialFolder.Personal); } }
+
+        public static string PathCombine(params string[] args)
+        {
+            if (args == null || args.Length == 0) throw new ArgumentException("args");
+            if (args.Length == 1) return args[0];
+            string res = args[0];
+            for (int i = 1; i < args.Length; i++)
+                res = Path.Combine(res, args[i]);
+            return res;
+        }
+
+        public static string PathResolve(string path)
+        {
+            if (path == null) throw new ArgumentNullException("path");
+            if (path == "~") return HomePath;
+            // there's currently no cross platform way to get 
+            // another users' homedir, so we don't support that
+            // yet.
+
+            if (path.StartsWith("~" + Path.DirectorySeparatorChar.ToString()))
+            {
+                path = Path.Combine(HomePath, path.Substring(2));
+            }
+            return path;
+        }
+
+        public static string PathRelative(string path, string relativeto)
+        {
+            // already relative? 
+            if (!Path.IsPathRooted(path))
+                return path;
+
+            // do this before tolower
+            string result = Path.GetFileName(path);
+
+            // not on same drive? 
+            if (string.Compare(Path.GetPathRoot(path), Path.GetPathRoot(relativeto), true) != 0)
+                return path;
+
+            path = Path.GetDirectoryName(path);
+
+            while (!relativeto.StartsWith(
+                path.EndsWith(Path.DirectorySeparatorChar.ToString()) ?
+                path: path + Path.DirectorySeparatorChar))
+            {
+                result = Path.Combine(Path.GetFileName(path), result);
+                path = Path.GetDirectoryName(path);
+            }
+
+            relativeto = Path.GetDirectoryName(relativeto);
+
+            while (relativeto != path)
+            {
+                result = Path.Combine("..", result);
+                relativeto = Path.GetDirectoryName(relativeto);
+            }
+            return result;
+        }
+    }
+}
